37ef7ddc30e20a08043a2967f35d0e3734109da9
suhyunking <kksshh09211@gmail.com>
2022-12-28 23:45:38 +0900

    회원서비스 테스트 구현중

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,13 +4,56 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
+    // http://localhost:8081/hello -> 안녕하세요. hello!!
     @GetMapping("hello")
     public String hello(Model model) {
         model.addAttribute("data", "hello!!");
         return "hello";
     }
+
+
+    // http://localhost:8081/hello-mvc?name=spring!!!!! -> hellospring!!!!!
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+
+    // http://localhost:8081/hello-string?name=spring!!!!! -> hello spring!!!!!
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name; //문자열 그대로 출력
+    }
+
+
+    //json 방식
+    // http://localhost:8081/hello-api?name=spring!!!!! -> {"name":"spring!!!!!"}
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
 }
